374741a5cce83d25a260e668398b3119b183ac9a
VictorGonzalezGomez <v.gonzalezgomez.vg@gmail.com>
2023-01-16 03:44:28 -0300

    add:inserting the reportRequest into every path

--- src/routers/ jewelryRoutes.js
+++ src/routers/ jewelryRoutes.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const { gettingAllJewelry, filteredJewelry }= require("../controllers/jawelryController");
+const {reportRequest}= require("../middlewares/logger");
 
-
-router.get('/joyas', gettingAllJewelry);
-router.get('/joyas/filtros',filteredJewelry);
+router.get('/joyas', gettingAllJewelry,reportRequest);
+router.get('/joyas/filtros',filteredJewelry,reportRequest);
 
 router.get("*", (req,res) =>{
     res.status(404).send("ERROR 404 PAGE NOT FOUND");
